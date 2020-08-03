5e4b1ec7eefafdcecfbba682cf957162ee35905d
natipoi <yota@natipoi.com>
2020-08-03 20:49:54 +0900

    m

--- pages/index.vue
+++ pages/index.vue
@@ -234,7 +234,15 @@
       <h2 class="h2 side-space gray">socoのニュース</h2>
       <p :class="{ 'catch-phrase side-space': true }">日々、形作られていく、<ruby><rb>soco</rb><rp>（</rp><rt>ソコ</rt><rp>）</rp></ruby>。</p>
     <div class="blogs side-space">
-
+    <Stories
+      v-for="post in posts"
+      :title="post.fields.title"
+      :id="post.sys.id"
+      :publish="post.fields.publishDate"
+      :slug="post.fields.slug"
+      :image="post.fields.heroImage.fields.file.url"
+      :alt="post.fields.heroImage.fields.description"
+    />
     </div>
     <div class="section-bottom-button">
       <a href="/stories/">ニュースをもっと見る</a>
@@ -426,6 +434,50 @@ animation: slide 25s ease-out 0s infinite normal backwards running;
 </style>
 
 <script>
+  import Stories from '~/components/Stories.vue'
+  import {createClient} from '~/plugins/contentful.js'
+
+  const client = createClient()
+
+  export default {
+    props: {
+        active: {
+            type: Boolean,
+            default: false
+        }
+    },
+    components: {
+      Stories
+    },
+    asyncData ({env}) {
+      return Promise.all([
+        // fetch the owner of the blog
+        client.getEntries({
+          'sys.id': env.CTF_PERSON_ID
+        }),
+        // fetch all blog posts sorted by creation date
+        client.getEntries({
+          'content_type': env.CTF_BLOG_POST_TYPE_ID,
+          order: '-sys.createdAt'
+        })
+      ]).then(([entries, posts]) => {
+        // return data that should be available
+        // in the template
+        for (var post of posts.items) {
+        　var date = new Date(post.fields.publishDate);
+          var year = date.getFullYear();
+          var month = date.getMonth() + 1;
+          var day = date.getDate();
+          post.fields.publishDate = `${year}/${month}/${day}`
+
+        }
+        return {
+          person: entries.items[0],
+          posts: posts.items,
+        }
+      }).catch(console.error)
+    }
+  }
 
   if (process.client) {
 
