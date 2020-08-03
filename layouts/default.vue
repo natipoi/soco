f11e5b1ac1dba683cccf55587bb9ad6c635b9f1b
natipoi <yota@natipoi.com>
2020-08-03 21:53:05 +0900

    m

--- layouts/default.vue
+++ layouts/default.vue
@@ -145,6 +145,14 @@ export default {
   },
   mounted() {
     Typekit.load({async: true})
+    if (process.client) {
+      const japanStandardTime = new Date().toLocaleString({ timeZone: 'Asia/Tokyo' });
+      const hour = new Date(japanStandardTime).getHours();
+      // 5時〜18時の間はお昼モード
+      if ( hour >= 5 && hour < 18 ) {
+        this.active =  false
+      }
+    }
   },
   methods: {
     onClick() {
