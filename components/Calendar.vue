2153bfe55eef35fe7fc15d74043c33d7522ceaaa
natipoi <yota@natipoi.com>
2020-12-23 07:40:43 +0900

    更新

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -218,7 +218,7 @@
                     v-for="index in 15"
                     :key="index"
                     class="event-details-time-box">
-                    <div v-show="day_events[`${index + 9}`]" :class="{'popup-event': true}">
+                    <div v-if="day_events[`${index + 9}`]" :class="{'popup-event': true}">
                         <nuxt-link
                             :to="`/events/` + day_events[`${index + 9}`].get('slug')"
                             :class="{ 
